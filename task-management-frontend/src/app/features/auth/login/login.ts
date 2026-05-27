c306f7853aadcc489b9132b71f5409e860efff7f
isalip16 <Isali.P@digitustec.com.au>
2026-05-27 16:29:24 +0530

    feat(auth): implement login page with reactive form validation

--- task-management-frontend/src/app/features/auth/login/login.ts
+++ task-management-frontend/src/app/features/auth/login/login.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: '../auth.scss',
 })
-export class Login {}
+export class Login implements OnInit {
+  loginForm!: FormGroup;
+  isLoading = false;
+  errorMessage = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group ({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  get f(){
+    return this.loginForm.controls;
+  }
+
+  onSubmit(){
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.errorMessage = err.error?.message || 'Login failed. Please try again.';
+        this.isLoading = false;
+      }
+    })
+  }
+}
