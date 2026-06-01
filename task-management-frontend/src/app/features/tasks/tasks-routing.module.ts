f53764ab7d17b93f3800bddc8414797aad8f6750
isalip16 <Isali.P@digitustec.com.au>
2026-06-01 08:55:28 +0530

    feat(tasks): configure tasks routing module with list, create and edit routes

--- task-management-frontend/src/app/features/tasks/tasks-routing.module.ts
+++ task-management-frontend/src/app/features/tasks/tasks-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskList } from './task-list/task-list.component';
 import { TaskForm } from './task-form/task-form.component';
 
 const routes: Routes = [
-  { path: '', component: TaskForm }
+  { path: '', component: TaskList },        // /tasks
+  { path: 'new', component: TaskForm },     // /tasks/new?projectId=xxx
+  { path: ':id/edit', component: TaskForm } // /tasks/:id/edit
 ];
 
 @NgModule({
